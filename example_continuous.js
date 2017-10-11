b6d2c79c03cc9399bcd821342e5d971590b7caf2
Jay Fialkowski <jfialkowski@manh.com>
2017-10-11 10:47:10 -0400

    clean up example

--- example_continuous.js
+++ example_continuous.js
@@ -2,20 +2,19 @@ var nflapi = require(__dirname + '/index.js');
 
 let options = { // options and all properties are optional
 	concurrent: 1, // Number of simultaneous network requests to allow (Default: 1)
-	cycle: 1000 * 60 * 3, // Delay (in milliseconds) before repeating. Do not provide or set to -1 for no cycle (Default: -1)
 	filters: {
 		include: ['BUF'],
 		in_progress: false
 	}
 }
-
-fetch(options, 1000 * 5);
+let delay = 1000 * 5 // 5 seconds
+fetch(options, delay);
 
 /**
  * fetch - Call the API's fetch function, passing in options and the interval between cycles (in ms)
  * 
- * @param {any} options 
- * @param {any} interval 
+ * @param {object} options 
+ * @param {number} interval 
  */
 function fetch(options, interval) {
 	nflapi.fetch(options).subscribe(
